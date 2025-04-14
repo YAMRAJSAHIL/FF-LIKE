f4af7f567c09d5ba84a4ceb965db0761d19de38b
YAMRAJSAHIL <royalgamers2010@gmail.com>
2025-04-14 21:37:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -234,5 +234,4 @@ def handle_requests():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
-    host="0.0.0.0" , port=5000
+    app.run(debug=True, use_reloader=False, host="0.0.0.0" , port=5000)
